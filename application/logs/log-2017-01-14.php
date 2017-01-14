ec8f1852432e597869469468339a6a7459697211
bakasajoshua <baksajoshua09@gmail.com>
2017-01-14 19:15:29 +0300

    second push

---
+++ application/logs/log-2017-01-14.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-01-14 14:22:49 --> Config Class Initialized
+INFO - 2017-01-14 14:22:49 --> Hooks Class Initialized
+DEBUG - 2017-01-14 14:22:51 --> UTF-8 Support Enabled
+INFO - 2017-01-14 14:22:51 --> Utf8 Class Initialized
+INFO - 2017-01-14 14:22:51 --> URI Class Initialized
+DEBUG - 2017-01-14 14:22:54 --> No URI present. Default controller set.
+INFO - 2017-01-14 14:22:54 --> Router Class Initialized
+INFO - 2017-01-14 14:22:54 --> Output Class Initialized
+INFO - 2017-01-14 14:22:54 --> Security Class Initialized
+DEBUG - 2017-01-14 14:22:54 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-01-14 14:22:55 --> Input Class Initialized
+INFO - 2017-01-14 14:22:55 --> Language Class Initialized
+INFO - 2017-01-14 14:22:57 --> Language Class Initialized
+INFO - 2017-01-14 14:22:57 --> Config Class Initialized
+INFO - 2017-01-14 14:22:57 --> Loader Class Initialized
+INFO - 2017-01-14 14:23:00 --> Helper loaded: url_helper
+INFO - 2017-01-14 14:23:00 --> Helper loaded: file_helper
+INFO - 2017-01-14 14:23:04 --> Database Driver Class Initialized
+INFO - 2017-01-14 14:23:05 --> Email Class Initialized
+INFO - 2017-01-14 14:23:06 --> Session: Class initialized using 'files' driver.
+INFO - 2017-01-14 14:23:11 --> Controller Class Initialized
+DEBUG - 2017-01-14 14:23:11 --> Home MX_Controller Initialized
+DEBUG - 2017-01-14 14:23:13 --> File loaded: C:\xampp\htdocs\leave\application\modules/Home/views/index_v.php
+DEBUG - 2017-01-14 14:23:13 --> File loaded: C:\xampp\htdocs\leave\application\views\template/template_v.php
+INFO - 2017-01-14 14:23:13 --> Final output sent to browser
+DEBUG - 2017-01-14 14:23:13 --> Total execution time: 26.8469
